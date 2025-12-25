8ba8f86d23b045a12ff41f6d47c4354fd8d01e4f
diatigui75 <diatiguifane3@gmail.com>
2025-12-25 14:28:31 +0100

    ✨ F01: LOGIN > Add title

---
+++ src/components/pages/LoginPage.jsx
@@ -0,0 +1,33 @@
+
+import { useState } from "react"
+export default function LoginPage() {
+    // États (state, données)
+    const [inputValue, setInputValue] = useState("")
+
+
+
+    // Comportements
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        alert(`Bonjour, ${inputValue} `)
+        setInputValue("")
+    }
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    // Affichage (render)
+
+    return (
+        <div>
+            <h1>Bienvenue chez nous !</h1> <br />
+            <h2>Connectez-vous</h2>
+            <form action="submit" onSubmit={handleSubmit}>
+                <input value={inputValue} onChange={handleChange} type="text" placeholder="Entrez votre prénom..." required />
+                <button type="submit">Accéder à votre espace</button>
+            </form>
+        </div>
+    )
+}
